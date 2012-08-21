4a4ee761cd1afc1d4fb97bd83fb26851af261fee
Christopher-Barham-AKQA <Christopher.Barham@AKQA.com>
2012-08-21 14:29:59 +0200

    init

---
+++ src/main/java/com/akqa/service/ThirtyPercentFailService/Service.java
@@ -0,0 +1,41 @@
+
+package com.akqa.service.ThirtyPercentFailService;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * A 'Service' returns true 3/4 of the time to indicate success, and false 1/3 of the time in order to demonstrate
+ * failure
+ * 
+ * @author Christopher.Barham@AKQA.com
+ * @since 21 August 2012
+ * 
+ */
+public class Service {
+    static final int DEFAULT_LIMIT = 100000;
+    AtomicInteger counter;
+    int limit;
+    int threeQuartersOfLimit;
+
+    public Service() {
+        this(new AtomicInteger(), DEFAULT_LIMIT);
+    }
+
+    public Service(AtomicInteger counter, int limit) {
+        super();
+        this.limit = limit;
+        threeQuartersOfLimit = (int) (0.75 * limit);
+        this.counter = counter;
+    }
+
+    public boolean serviceCall() {
+            if (counter.incrementAndGet() > threeQuartersOfLimit) {
+                if (counter.get() >= limit) {
+                    counter.set(0);
+                }
+                return false;
+            }
+        return true;
+    }
+}
---
+++ src/test/java/com/akqa/service/ThirtyPercentFailService/ServiceTest.java
@@ -0,0 +1,55 @@
+
+package com.akqa.service.ThirtyPercentFailService;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Junit test
+ * 
+ * @author Christopher.Barham@AKQA.com
+ * @since 21 August 2012
+ */
+
+public class ServiceTest {
+
+    private Service service;
+
+    @Before
+    public void setUp() throws Exception {
+        service = new Service();
+    }
+
+    @Test
+    public void testFour() {
+        service = new Service(new AtomicInteger(), 3);
+        assertThat("first call should be true", service.serviceCall(), is(true));
+        assertThat("second call should be true", service.serviceCall(), is(true));
+        assertThat("third call should be false", service.serviceCall(), is(false));
+        assertThat("fourth call should be true", service.serviceCall(), is(true));
+    }
+
+    @Test
+    public void testLots() {
+        int limit = 1000;
+        int passed = 0;
+        int failed = 0;
+
+        service = new Service(new AtomicInteger(), limit);
+        for (int i = 0; i < 2 * limit; i++) {
+            if (service.serviceCall()) {
+                passed++;
+            } else {
+                failed++;
+            }
+        }
+        assertThat(passed,is(1500));
+        assertThat(failed,is(500));
+    }
+}
